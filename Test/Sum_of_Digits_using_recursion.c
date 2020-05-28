67b11dcf1304ca16485c9110ebfba4fe1e541b9d
Allwyn Wesly S <weslyallwyn@gmail.com>
2020-05-28 11:31:31 +0530

    Create Sum_of_Digits_using_recursion.c

---
+++ Test/Sum_of_Digits_using_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int addition(int num);
+int main()
+{
+    int num, sum;
+    printf("Enter any number to find sum of digits: ");
+    scanf("%d", &num);
+    sum = addition(num);
+    printf("Sum of digits of %d = %d", num, sum);
+    return 0;
+}
+int addition(int num)
+{
+    // Base condition
+    if(num == 0)
+        return 0;
+    return ((num % 10) + addition(num / 10));
+}
